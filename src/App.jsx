2e657cdaed5fef57918e2d857c95d6230d90bc2c
Ana Maria <ana.msilva@hotmail.com>
2026-06-18 20:58:01 -0300

    feat: add um array de objeto chamado tema para lista suspensa do formuario

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import { FormularioDeEvento } from './componentes/FormularioDeEvento'
 
 
 function App() {
+
+  const temas = [
+    {id: 1, nome: 'front-end'},
+    {id: 2, nome: 'back-end'},
+    {id: 3, nome: 'devops'},
+    {id: 4, nome: 'inteligencia artifical'},
+    {id: 5, nome: 'data science'},
+    {id: 6, nome: 'cloud'}
+  ]
+
   return (
     <main>
       <header>
